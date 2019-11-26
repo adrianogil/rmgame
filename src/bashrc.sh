63ec5a5c9b0dfb88fef64233fd120dea8b04979d
Adriano Gil <adrianogil.san@gmail.com>
2019-11-26 15:02:14 -0400

    Setup as gil-project

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -1,2 +1,9 @@
-export RMGAME_DATA_DIR=$RMGAME_DIR/data/
-alias txtmgame="python3 $RMGAME_DIR/game.py"
+export RMGAME_DATA_DIR=$RMGAME_DIR/../data/
+
+if [ -z "$RMGAME_PYTHON_PATH" ]
+then
+    export RMGAME_PYTHON_PATH=$RMGAME_DIR/python/
+    export PYTHONPATH=$RMGAME_PYTHON_PATH:$PYTHONPATH
+fi
+
+alias txtmgame="python3 $RMGAME_DIR/python/rmgame/game.py"
